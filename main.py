b26b5ef8658f5212cd38574114f8d2224282b58f
SiwooPak <siwoo1620@gmail.com>
2021-05-31 07:10:36 +0900

    main.py 수정 복원

--- main.py
+++ main.py
@@ -11,12 +11,9 @@ lst = []
 cnt = 0;
 
 for i in feed['entries']:
-    if cnt < 5:	
-    	dt = datetime.datetime.strptime(i['published'], '%a, %d %b %Y %H:%M:%S %Z')
-    	markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
-    	print(i['link'], i['title'])
-	
-	cnt++
+    dt = datetime.datetime.strptime(i['published'], '%a, %d %b %Y %H:%M:%S %Z')
+    markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
+    print(i['link'], i['title'])
     
 
 f = open("README.md",mode="w", encoding="utf-8")
