930be52050b699a94ca4f55ce0a3e45c448fad6b
SiwooPak <siwoo1620@gmail.com>
2021-05-31 07:02:16 +0900

    modified: 최근 업로드된 5개만

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ markdown_text = """# 시우의 블로그 업뎃상황!
 ## Recent blog posts
 
 lst = []
-
+cnt = 0;
 
 for i in feed['entries']:
-    dt = datetime.datetime.strptime(i['published'], '%a, %d %b %Y %H:%M:%S %Z')
-    markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
-    print(i['link'], i['title'])
+    if cnt < 5:	
+    	dt = datetime.datetime.strptime(i['published'], '%a, %d %b %Y %H:%M:%S %Z')
+    	markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
+    	print(i['link'], i['title'])
+	cnt++
+    
 
 f = open("README.md",mode="w", encoding="utf-8")
 f.write(markdown_text)
