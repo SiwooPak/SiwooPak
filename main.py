666896ba1689c037ec0aae50d3db9b0c1912c5cb
SiwooPak <siwoo1620@gmail.com>
2021-05-31 07:50:44 +0900

    modified: README.md 수정

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import feedparser, datetime
 velog_uri="https://v2.velog.io/rss/dev_shu"
 feed = feedparser.parse(velog_uri)
 
-markdown_text = "# 시우의 블로그 업뎃상황!"
+markdown_text = "# 시우의 블로그 업뎃상황!<br/>"
 
 ## Recent blog posts
 
